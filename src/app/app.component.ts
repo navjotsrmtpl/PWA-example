f6b60e93c94ca60db02ea1ba3957dc6826c3a64c
Navjot <navjotsingh1795@gmail.com>
2019-10-06 23:53:19 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import {SwUpdate} from '@angular/service-worker';
+import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwa';
+
+update:boolean= false
+  testData: any;
+  constructor(updates:SwUpdate,private data: DataService){
+
+      updates.available.subscribe(event=>{
+          // this.update= true;
+
+          updates.activateUpdate().then(()=>document.location.reload());
+      })
+  }
+
+  ngOnInit(){
+    this.data.testApi().subscribe((res)=>{
+        this.testData = res;
+    })
+  }
 }
